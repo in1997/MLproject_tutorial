00fc4b5ef81a5fa8ec93313484e1ae20182d3516
Yogansh Sharma <sharmayogansh@gmail.com>
2026-04-18 16:47:45 +0530

    Hyperparameter tuning added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -44,8 +44,44 @@ class ModelTrainer:
                 "XGB Regressor": xgboost.XGBRegressor(),
                 "CatBoost Regressor": catboost.CatBoostRegressor(verbose=False)
             }
+            params = {
+                "Decision Tree Regressor": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson']
+                },
+                "Random Forest Regressor": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Gradient Boosting Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Linear Regression": {},
+                "Ridge Regression": {},
+                "Lasso Regression": {},
+                "AdaBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "Support Vector Regressor": {
+                    'kernel': ['linear', 'poly', 'rbf', 'sigmoid']
+                },
+                "K-Neighbors Regressor": {
+                    'n_neighbors': [5, 7, 9, 11],
+                    'weights': ['uniform', 'distance']
+                },
+                "XGB Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoost Regressor": {
+                    'learning_rate': [.1, .01, .05, .001],
+                    'depth': [6, 8, 10],
+                    'iterations': [30, 50, 100]
+                }
+            }
 
-            model_report: dict = evaluate_models(x_train, y_train, x_test, y_test, models)
+            model_report: dict = evaluate_models(x_train, y_train, x_test, y_test, models, params=params)
 
             logging.info("model report generated")
 
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 from src.logger import logging
 import dill
@@ -21,12 +23,18 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(x_train, y_train, x_test, y_test, models):
+def evaluate_models(x_train, y_train, x_test, y_test, models, params):
     try:
         report = {}
 
         for i in range(len(models)):
             model = list(models.values())[i]
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=5)
+            gs.fit(x_train, y_train)
+            model.set_params(**gs.best_params_)
+
             model.fit(x_train, y_train)
 
             y_test_pred = model.predict(x_test)
